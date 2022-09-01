10adb3c535852ce9b3b6ef9bc36a65bacdc27a1b
Nadia Tariq <73836438+nadiatariq@users.noreply.github.com>
2022-09-01 09:54:22 +0500

    realm-Crud-code
    Search bar becomes not empty after search

--- DBFormForStudent/FetchListViewController.swift
+++ DBFormForStudent/FetchListViewController.swift
@@ -7,35 +7,39 @@
 
 import UIKit
 import RealmSwift
-class FetchListViewController: UIViewController,UITableViewDelegate,UITableViewDataSource, UISearchBarDelegate {
+class FetchListViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UISearchBarDelegate {
+    
+    @IBOutlet weak var mySearchText: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
     
     var getRealmData :  [Student] = []
-    // create a variable of array of student type
-    var searchGetRealmData :  [Student] = []
-    
+    var searchGetRealmData = [Student]()
     var isSearch = false
     var currentSelectedRow = Student()
     var pervious = ViewController()
+    var orignalData = [Student]()
     
-    var searchResults = try! Realm().objects(Student)
-  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         fetchData()
         tableView.reloadData()
-        
+    
         self.navigationItem.rightBarButtonItem = self.editButtonItem
-        // Do any additional setup after loading the view.
+        
     }
     
-    @IBOutlet weak var mySearchText: UISearchBar!
-    @IBOutlet weak var tableView: UITableView!
+    func fetchData(){
+        let realm = try! Realm()
+        getRealmData = Array(realm.objects(Student.self))
+      
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        if isSearch{
+        if isSearch {
             return searchGetRealmData.count
-            
         }
         else{
             return getRealmData.count
@@ -68,8 +72,9 @@ class FetchListViewController: UIViewController,UITableViewDelegate,UITableViewD
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
-        return 50.0
+        return UITableView.automaticDimension
     }
+    
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool
     {
         // Return `false` if you do not want the
@@ -125,44 +130,37 @@ class FetchListViewController: UIViewController,UITableViewDelegate,UITableViewD
             self.pervious.callbackData(sendBack)
         }
         
-        
-    }
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        var searchResults = try! Realm().
-        self.isSearch = true
-        //        self.SearchgetRealmData = self.getRealmData.filter({$0.name == searchText})
-        
-        self.searchGetRealmData = self.getRealmData.filter({$0.name?.lowercased().contains( (mySearchText!.text?.lowercased())! ) as! Bool })
-        self.tableView.reloadData()
-        
     }
     
-    func filterResultsWithSearchString(searchString: String) {
-      let predicate = NSPredicate(format: "name BEGINSWITH [c]%@", searchString) // 1
-      let scopeIndex = searchController.searchBar.selectedScopeButtonIndex // 2
-      let realm = try! Realm()
-
-      switch scopeIndex {
-      case 0:
-        searchResults = realm.objects(Student).filter(predicate).sorted("name", ascending: true) // 3
-      case 1:
-        searchResults = realm.objects(Student).filter(predicate).sorted("created", ascending: true) // 4
-      default:
-        searchResults = realm.objects(Student).filter(predicate) // 5
-      }
-    }
-    func fetchData(){
-        let realm = try! Realm()
-        getRealmData = Array(realm.objects(Student.self))
-        for studentData in getRealmData{
-            print("\(studentData.name ?? " " ) and rollNumber is \(studentData.rollNo)")
+   func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            if mySearchText.text == nil || mySearchText.text == "" {
             
-        }
-    }
-    
+                        isSearch = false
+                        view.endEditing(true)
+                        tableView.reloadData()
+                    }else{
+                        isSearch = true
+                        let resultPredicate = NSPredicate(format: "name CONTAINS[c] %@",  searchText)
+                            self.searchGetRealmData = getRealmData.filter{resultPredicate.evaluate(with: $0)}
+                        tableView.reloadData()
+                        }
+   }
     @IBAction func backActionButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+      }
+ 
     }
+
+
+
+
+    //        self.SearchgetRealmData = self.getRealmData.filter({$0.name == searchText})
     
-}
+    //        let formateString = "name contains[cd] %@"
+    //        var filterList = searchGetRealmData.filter { NSPredicate(format: formateString, searchText).evaluate(with: $0)} as NSArray
+
+    
+    
+
+
 
